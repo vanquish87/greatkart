b2d867734b0cdbf6dde3dc351a8689dbb3b73384
Jasmeet Singh <jasmeet.singh@inspiraenterprise.com>
2022-07-31 08:54:16 +0530

    updated gitignore to ignore media folder

--- greatkart/views.py
+++ greatkart/views.py
@@ -4,6 +4,7 @@ from store.models import Product
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
+    # products = get_list_or_404(Product, is_available=True)
 
     context = {'products': products}
     return render(request, 'home.html', context)
